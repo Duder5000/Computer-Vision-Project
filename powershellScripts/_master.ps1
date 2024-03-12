ca54247560ddbd9129b018896cd470b8de52059e
Duder <soames.alex@gmail.com>
2024-03-12 08:43:00 -0700

    *

--- powershellScripts/_master.ps1
+++ powershellScripts/_master.ps1
@@ -1,16 +1,15 @@
 $folderPath = "G:\481\Computer-Vision-Project\powershellScripts\"
 
 & "$folderPath\jsonToTxt.ps1"
-Write-Host "Sleep 30s"
-Start-Sleep -Seconds 30
+Write-Host "jsonToTxt - Sleep 20s"
+Start-Sleep -Seconds 20
 
 & "$folderPath\getDims.ps1"
-Write-Host "Sleep 30s"
-Start-Sleep -Seconds 30
-& "$folderPath\jsonToTxt.ps1"
+Write-Host "getDims - Sleep 20s"
+Start-Sleep -Seconds 20
 
 & "$folderPath\combineTheTxts.ps1"
-Write-Host "Sleep 30s"
-Start-Sleep -Seconds 30
+Write-Host "combineTheTxts - Sleep 20s"
+Start-Sleep -Seconds 20
 
 & "$folderPath\RemoveBlankLines.ps1"
