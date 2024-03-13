be5a07ca4f98eeff43cda9f2a7c0b0030790359f
elinanessar <elinanessar@gmail.com>
2024-03-12 17:14:04 -0700

    working on _get_circle_shape_yolo_object

--- Labelme2YOLO-main/labelme2yolo.py
+++ Labelme2YOLO-main/labelme2yolo.py
@@ -140,52 +140,104 @@ class Labelme2YOLO(object):
             
         return yolo_obj_list
     
-    def _get_circle_shape_yolo_object(self, shape, img_h, img_w):
-        label_id = self._label_id_map[shape['label']]
-        obj_center_x, obj_center_y = shape['points'][0]
+    # def _get_circle_shape_yolo_object(self, shape, img_h, img_w):
+    #     label_id = self._label_id_map[shape['label']]
+    #     obj_center_x, obj_center_y = shape['points'][0]
 
-        radius = math.sqrt((obj_center_x - shape['points'][1][0]) ** 2 +
-                           (obj_center_y - shape['points'][1][1]) ** 2)
+    #     radius = math.sqrt((obj_center_x - shape['points'][1][0]) ** 2 +
+    #                        (obj_center_y - shape['points'][1][1]) ** 2)
 
-        if self._to_seg:
-            retval = [label_id]
+    #     if self._to_seg:
+    #         retval = [label_id]
 
-            n_part = radius / 10
-            n_part = int(n_part) if n_part > 4 else 4
-            n_part2 = n_part << 1
+    #         n_part = radius / 10
+    #         n_part = int(n_part) if n_part > 4 else 4
+    #         n_part2 = n_part << 1
 
-            pt_quad = [None for i in range(0, 4)]
-            pt_quad[0] = [[obj_center_x + math.cos(i * math.pi / n_part2) * radius,
-                         obj_center_y - math.sin(i * math.pi / n_part2) * radius]
-                         for i in range(1, n_part)]
-            pt_quad[1] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[0]]
-            pt_quad[1].reverse()
-            pt_quad[3] = [[x1, obj_center_y * 2 - y1] for x1, y1 in pt_quad[0]]
-            pt_quad[3].reverse()
-            pt_quad[2] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[3]]
-            pt_quad[2].reverse()
+    #         pt_quad = [None for i in range(0, 4)]
+    #         pt_quad[0] = [[obj_center_x + math.cos(i * math.pi / n_part2) * radius,
+    #                      obj_center_y - math.sin(i * math.pi / n_part2) * radius]
+    #                      for i in range(1, n_part)]
+    #         pt_quad[1] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[0]]
+    #         pt_quad[1].reverse()
+    #         pt_quad[3] = [[x1, obj_center_y * 2 - y1] for x1, y1 in pt_quad[0]]
+    #         pt_quad[3].reverse()
+    #         pt_quad[2] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[3]]
+    #         pt_quad[2].reverse()
 
-            pt_quad[0].append([obj_center_x, obj_center_y - radius])
-            pt_quad[1].append([obj_center_x - radius, obj_center_y])
-            pt_quad[2].append([obj_center_x, obj_center_y + radius])
-            pt_quad[3].append([obj_center_x + radius, obj_center_y])
+    #         pt_quad[0].append([obj_center_x, obj_center_y - radius])
+    #         pt_quad[1].append([obj_center_x - radius, obj_center_y])
+    #         pt_quad[2].append([obj_center_x, obj_center_y + radius])
+    #         pt_quad[3].append([obj_center_x + radius, obj_center_y])
 
-            for i in pt_quad:
-                for j in i:
-                    j[0] = round(float(j[0]) / img_w, 6)
-                    j[1] = round(float(j[1]) / img_h, 6)
-                    retval.extend(j)
-            return retval
+    #         for i in pt_quad:
+    #             for j in i:
+    #                 j[0] = round(float(j[0]) / img_w, 6)
+    #                 j[1] = round(float(j[1]) / img_h, 6)
+    #                 retval.extend(j)
+    #         return retval
 
-        obj_w = 2 * radius
-        obj_h = 2 * radius
+    #     obj_w = 2 * radius
+    #     obj_h = 2 * radius
         
-        yolo_center_x= round(float(obj_center_x / img_w), 6)
-        yolo_center_y = round(float(obj_center_y / img_h), 6)
-        yolo_w = round(float(obj_w / img_w), 6)
-        yolo_h = round(float(obj_h / img_h), 6)
+    #     yolo_center_x= round(float(obj_center_x / img_w), 6)
+    #     yolo_center_y = round(float(obj_center_y / img_h), 6)
+    #     yolo_w = round(float(obj_w / img_w), 6)
+    #     yolo_h = round(float(obj_h / img_h), 6)
 
-        return label_id, yolo_center_x, yolo_center_y, yolo_w, yolo_h
+    #     return label_id, yolo_center_x, yolo_center_y, yolo_w, yolo_h
+
+    def _get_circle_shape_yolo_object(self, shape, img_h, img_w):
+    label_id = self._label_id_map[shape['label']]
+    obj_center_x, obj_center_y = shape['points'][0]
+
+    # Calculate radius
+    radius = math.sqrt((obj_center_x - shape['points'][1][0]) ** 2 +
+                       (obj_center_y - shape['points'][1][1]) ** 2)
+
+    if self._to_seg:
+        retval = [label_id]
+
+        n_part = radius / 10
+        n_part = int(n_part) if n_part > 4 else 4
+        n_part2 = n_part << 1
+
+        pt_quad = [None for i in range(0, 4)]
+        pt_quad[0] = [[obj_center_x + math.cos(i * math.pi / n_part2) * radius,
+                        obj_center_y - math.sin(i * math.pi / n_part2) * radius]
+                        for i in range(1, n_part)]
+        pt_quad[1] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[0]]
+        pt_quad[1].reverse()
+        pt_quad[3] = [[x1, obj_center_y * 2 - y1] for x1, y1 in pt_quad[0]]
+        pt_quad[3].reverse()
+        pt_quad[2] = [[obj_center_x * 2 - x1, y1] for x1, y1 in pt_quad[3]]
+        pt_quad[2].reverse()
+
+        pt_quad[0].append([obj_center_x, obj_center_y - radius])
+        pt_quad[1].append([obj_center_x - radius, obj_center_y])
+        pt_quad[2].append([obj_center_x, obj_center_y + radius])
+        pt_quad[3].append([obj_center_x + radius, obj_center_y])
+
+        for i in pt_quad:
+            for j in i:
+                j[0] = round(float(j[0]) / img_w, 6)
+                j[1] = round(float(j[1]) / img_h, 6)
+                retval.extend(j)
+        return retval
+
+    # Calculate bounding box coordinates
+    x_min = max(0, obj_center_x - radius)
+    y_min = max(0, obj_center_y - radius)
+    x_max = min(img_w, obj_center_x + radius)
+    y_max = min(img_h, obj_center_y + radius)
+
+    # Normalize bounding box coordinates
+    yolo_center_x = round(float((x_min + x_max) / (2 * img_w)), 6)
+    yolo_center_y = round(float((y_min + y_max) / (2 * img_h)), 6)
+    yolo_width = round(float((x_max - x_min) / img_w), 6)
+    yolo_height = round(float((y_max - y_min) / img_h), 6)
+
+    return label_id, yolo_center_x, yolo_center_y, yolo_width, yolo_height
 
     def _get_other_shape_yolo_object(self, shape, img_h, img_w):
         label_id = self._label_id_map[shape['label']]
